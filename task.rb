e395cc1dc317e14b5e5b8f181869ab1054ffc2ac
YoheiNamiki <ynamiki.1989@gmail.com>
2020-09-15 14:09:26 +0900

    revised 7,8

--- task.rb
+++ task.rb
@@ -56,7 +56,7 @@ end
 def q7
   array = ["1", "2", "3", "4", "5"]
   # 以下に回答を記載
-  array.map! { |x| x.to_i}
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -65,8 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map! { |x| x.capitalize}
-  upper_case_programming_languages = programming_languages.map { |x| x.upcase}
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -76,7 +76,7 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-push.names.map 
+push.names.map
 end
 
 def q10
